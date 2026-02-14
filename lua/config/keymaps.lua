fe35a49f9c362792ef2cdb37fd1f5d2050e81c63
Alexandru Prisacariu <alex@alexprisacariu.dev>
2026-02-14 15:38:00 +0200

    File explorer cwd as default for <leader>e

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -18,3 +18,7 @@ keymap("n", "<C-h>", ":call WinMove('h')<CR>", opts)
 keymap("n", "<C-j>", ":call WinMove('j')<CR>", opts)
 keymap("n", "<C-k>", ":call WinMove('k')<CR>", opts)
 keymap("n", "<C-l>", ":call WinMove('l')<CR>", opts)
+
+keymap("n", "<leader>e", function()
+  Snacks.explorer()
+end, { noremap = true, silent = true, desc = "File Explorer (cwd)" })
--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -3,6 +3,8 @@ return {
     "folke/snacks.nvim",
     opts = {
       scroll = { enabled = false },
+      dashboard = { enabled = false },
+      indent = { animate = { enabled = false } },
     },
   },
 }
